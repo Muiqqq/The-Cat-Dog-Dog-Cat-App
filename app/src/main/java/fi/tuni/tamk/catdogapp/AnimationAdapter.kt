a192c05faa23aa6ea4fb0d65fd904809197cd8f4
Aleksi Muikku <aleksi.muikku@hotmail.com>
2021-05-19 17:57:01 +0300

    Add AnimationAdapter, adapter class for AnimationListener.

---
+++ app/src/main/java/fi/tuni/tamk/catdogapp/AnimationAdapter.kt
@@ -0,0 +1,11 @@
+package fi.tuni.tamk.catdogapp
+
+import android.view.animation.Animation
+
+open class AnimationAdapter : Animation.AnimationListener {
+    override fun onAnimationStart(animation: Animation?) {}
+
+    override fun onAnimationEnd(animation: Animation?) {}
+
+    override fun onAnimationRepeat(animation: Animation?) {}
+}
